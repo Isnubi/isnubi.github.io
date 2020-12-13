64586bad3899256adc14a3b543df725b2883ca0b
Isnubi <40593456+Isnubi@users.noreply.github.com>
2020-12-13 15:17:16 +0100

    Add files via upload

---
+++ assets/dist/js/background.js
@@ -0,0 +1,58 @@
+var random = Math.floor(Math.random() * 15);
+
+let programBg = [
+    "url('https://i.ibb.co/L0dVfqk/index-back.jpg')",
+    "url('https://i.ibb.co/sJycYzH/bg2.jpg')",
+    "url('https://i.ibb.co/rtjyXrs/bg3.jpg')",
+    "url('https://i.ibb.co/Ht4s0pq/bg4.jpg')",
+    "url('https://i.ibb.co/27Dn2yF/bg5.jpg')",
+    "url('https://i.ibb.co/fn6f8Y7/bg6.jpg')",
+    "url('https://i.ibb.co/b5fTrTj/Selective-focus-closeup-view-of-cables-plugged-into-a-server-network-panel-with-lights-indicating-ac.jpg')",
+    "url('https://i.ibb.co/p4F1sdh/bg8.jpg')",
+    "url('https://i.ibb.co/Hnjhbbw/bg9.jpg')",
+    "url('https://i.ibb.co/59Zggvy/bg10.jpg')",
+    "url('https://i.ibb.co/ccNW3CC/bg11.jpg')",
+    "url('https://i.ibb.co/dKWwqwG/bg12.jpg')",
+    "url('https://i.ibb.co/CtBhRd9/bg13.jpg')",
+    "url('https://i.ibb.co/hfgvL7q/bg14.jpg')",
+    "url('https://i.ibb.co/Kmz90ww/bg15.jpg')"];
+
+/* lien des images pour fedora
+let programBg = [
+    "url('/home/isnubi/Téléchargement/portfolio/album/background/index_back.jpg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg2.jpg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg3.jpg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg4.jpg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg5.jpg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg6.jpg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg7.jpg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg8.jpg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg9.jpg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg10.jpg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg11.jpg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg12.jpeg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg13.jpg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg14.jpg')",
+    "url('/home/isnubi/Téléchargement/album/background/bg15.jpg')"];
+*/
+
+/* lien des images pour le site sur github
+let programBg = [
+    "url('/isnubi.github.io/album/background/index_back.jpg')",
+    "url('/isnubi.github.io/album/background/bg2.jpg')",
+    "url('/isnubi.github.io/album/background/bg3.jpg')",
+    "url('/isnubi.github.io/album/background/bg4.jpg')",
+    "url('/isnubi.github.io/album/background/bg5.jpg')",
+    "url('/isnubi.github.io/album/background/bg6.jpg')",
+    "url('/isnubi.github.io/album/background/bg7.jpg')",
+    "url('/isnubi.github.io/album/background/bg8.jpg')",
+    "url('/isnubi.github.io/album/background/bg9.jpg')",
+    "url('/isnubi.github.io/album/background/bg10.jpg')",
+    "url('/isnubi.github.io/album/background/bg11.jpg')",
+    "url('/isnubi.github.io/album/background/bg12.jpeg')",
+    "url('/isnubi.github.io/album/background/bg13.jpg')",
+    "url('/isnubi.github.io/album/background/bg14.jpg')",
+    "url('/isnubi.github.io/album/background/bg15.jpg')"];
+*/
+
+document.getElementById("banner").style.backgroundImage=programBg[random];
